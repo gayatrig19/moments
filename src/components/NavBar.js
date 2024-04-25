9cb9595ef69a58ef14d1ca2c6f5cb0281a16e8b5
Gayatri Ghogare <ghogaregayatri@gmail.com>
2024-04-25 13:18:43 +0000

    Add NavBar burger toggle with a custom hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,13 @@ import { NavLink } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext';
 import Avatar from './Avatar';
 import axios from "axios";
+import useClickOutsideToggle from '../hooks/useClickOutsideToggle';
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -77,7 +80,12 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -85,19 +93,23 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink 
-              exact 
-              className={styles.NavLink} 
-              activeClassName={styles.Active} 
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
               to="/"
             >
-              <i className='fas fa-home'></i>Home
+              <i className="fas fa-home"></i>Home
             </NavLink>
 
-            {currentUser ? loggedInIcons : loggedOutIcons}   
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
